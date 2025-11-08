69d08c70d07d9f9e97f39728cd6318148d0135fa
GabriellCode <gabriellcc20061403@gmail.com>
2025-11-08 16:13:49 -0300

    addEslint/Prettier

--- lexia-constituicao-inteligente/eslint.config.js
+++ lexia-constituicao-inteligente/eslint.config.js
@@ -4,6 +4,9 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
+// 1. IMPORTAR O PRETTIER
+import eslintConfigPrettier from 'eslint-config-prettier'
+
 export default defineConfig([
   globalIgnores(['dist']),
   {
@@ -26,4 +29,7 @@ export default defineConfig([
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
+
+  // 2. ADICIONAR O PRETTIER COMO ÚLTIMO ITEM
+  eslintConfigPrettier,
 ])
