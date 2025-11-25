5ea74b296a50aa64f9002fc685120a54f953a78e
Gustavo Henrique Vasconcelos Alves De Queiroz <Gustavoh2029@gmail.com>
2025-11-25 19:05:12 -0300

    Correção do caminho da imagem

--- lexia-constituicao-inteligente/src/components/Footer.jsx
+++ lexia-constituicao-inteligente/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="bg-white text-black p-4 flex items-center">
-    <img src="/images/logo LexIA.png" alt="Logo LexIA" className="w-14 h-auto mr-4" />
+    <img src="/images/logo.png" alt="Logo LexIA" className="w-14 h-auto mr-4" />
     <p className="flex-1 text-center">© 2025 Constituição Inteligente. Todos os direitos reservados.</p>
   </footer>
 );
